61165227f7b733e0b466d4fa9c0fe0892dc69e70
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-08-20 13:02:41 +0200

    save

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ let count = 0;
 
 function increment() {
   count = count + 1;
-  document.getElementById("count-el").textContent=count;
+  document.getElementById("count-el").textContent = count;
 
+  console.log(count);
+}
+
+function save() {
   console.log(count);
 }
